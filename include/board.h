c9a9894d7c5c5abd0e22be1d433ef6f7b9640c7e
jinin-alj <jaoaljayyous.ieu2024@student.ie.edu>
2025-11-18 16:04:03 +0100

    progress from the first session

--- include/board.h
+++ include/board.h
@@ -1,1 +1,17 @@
- 
+#ifndef BOARD_H
+#define BOARD_H
+
+#define ROWS 6
+#define COLS 7
+
+typedef enum {
+    EMPTY,
+    PLAYER1,
+    PLAYER2
+} CellState;
+
+typedef struct {
+    CellState cells[ROWS][COLS];
+} Board;
+
+#endif // BOARD_H
